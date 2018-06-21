ad34baf7e4d8d8417340a7207e09ed2078262c00
Jiechao Wang <jiechao@starmobileinc.com>
2018-06-21 01:01:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/test/testaws/activities/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.test.testaws.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.example.test.testaws.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.setOnClickListener({ v ->
+            v.visibility = View.GONE
+            button.visibility = View.VISIBLE
+        })
+    }
+}
